e3c373043fb3186b23597fec5eca16313c8fae66
HansakaJS <hansakajayawarna@gmail.com>
2024-04-21 19:27:55 +0530

    update 23

--- RMS/src/main/java/com/interviewManagementApplication/RMS/constants/Consts.java
+++ RMS/src/main/java/com/interviewManagementApplication/RMS/constants/Consts.java
@@ -6,11 +6,12 @@ public interface Consts {
     //FTP server properties
 
     public final String server = "localhost";
-    public final String remoteDirectory = "/home/kpremarathne/Desktop/Trainings/Task14/files/";
+//    public final String remoteDirectory = "/home/hsudusinghe/Desktop/Trainings/Task14/files/";
+    public final String remoteDirectory = "/home/hsudusinghe/Desktop/Sample/";
 
-    public final String username = "kpremarathne";
+    public final String username = "hsudusinghe";
 
-    public final String password = "charuni40##K";
+    public final String password = "HansakaJayawarna123@";
 
     public final int port = 21;
 }
